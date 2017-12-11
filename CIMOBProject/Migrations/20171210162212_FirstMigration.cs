ea46c4f3996abd78e9f1ac456eb5c214da894056
Diogo Cruz <dmpcruz1997@gmail.com>
2017-12-11 18:26:04 +0000

    Eliminated some visual inconsistencies Eliminated some language inconsistencies Updated the Student Profile Layout Hid the Search Options for all Non-Employees Beautified the Search Options

--- CIMOBProject/Migrations/20171210162212_FirstMigration.cs
+++ CIMOBProject/Migrations/20171210162212_FirstMigration.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace CIMOBProject.Migrations
 {
-    public partial class FirstServerMigration : Migration
+    public partial class FirstMigration : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
