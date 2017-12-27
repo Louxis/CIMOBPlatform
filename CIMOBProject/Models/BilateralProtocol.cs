bce5e2ad1a9a5594abc27ff512d398fc993972d5
Zelxy <jcpereira.dev@gmail.com>
2017-12-27 21:11:35 +0000

    Added bilateralProcotol and Edital

---
+++ CIMOBProject/Models/BilateralProtocol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIMOBProject.Models
+{
+    public class BilateralProtocol
+    {
+        public int Id { get; set; }
+
+        public int SubjectId { get; set; }
+
+        public int CollegeId { get; set; }
+
+        public virtual CollegeSubject Subject { get; set; }
+
+        public virtual College College { get; set; }
+    }
+}
