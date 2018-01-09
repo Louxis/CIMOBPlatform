421c16335bbd6065a654f1203d59bf27d7cb260a
Jorge Moreira <32908579+Vexxenn@users.noreply.github.com>
2018-01-09 20:50:03 +0000

    Fixed the problem of testing multiple test classes at once.

--- XUnitTesting/ApplicationControllerTest.cs
+++ XUnitTesting/ApplicationControllerTest.cs
@@ -12,6 +12,7 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
 using System;
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 
 namespace XUnitTesting {
     public class ApplicationControllerTest
@@ -200,51 +201,51 @@ namespace XUnitTesting {
         }
 
 
-        [Fact]
-        public void TestWithFoxDriver()
-        {
-            using (var driver = new FirefoxDriver
-                  (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
-            {
-                driver.Navigate().GoToUrl
-                 (@"https://www.google.pt/");
-                /*IWebElement tableElement = driver.FindElement(By.TagName("tbody"));
-                IList<IWebElement> tableRow = tableElement.FindElements(By.TagName("tr"));
-                IList<IWebElement> rowTD;
-                foreach (IWebElement row in tableRow)
-                {
-                    rowTD = row.FindElements(By.TagName("td"));
-                    if (rowTD.Last().FindElement(By.Id("Details")) != null)
-                    {
-                        rowTD.Last().FindElement(By.Id("Details")).Click();
-                        break;
-                    }
-                }*/
-            }
-        }
+        //[Fact]
+        //public void TestWithFoxDriver()
+        //{
+        //    using (var driver = new FirefoxDriver
+        //          (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+        //    {
+        //        driver.Navigate().GoToUrl
+        //         (@"https://www.google.pt/");
+        //        /*IWebElement tableElement = driver.FindElement(By.TagName("tbody"));
+        //        IList<IWebElement> tableRow = tableElement.FindElements(By.TagName("tr"));
+        //        IList<IWebElement> rowTD;
+        //        foreach (IWebElement row in tableRow)
+        //        {
+        //            rowTD = row.FindElements(By.TagName("td"));
+        //            if (rowTD.Last().FindElement(By.Id("Details")) != null)
+        //            {
+        //                rowTD.Last().FindElement(By.Id("Details")).Click();
+        //                break;
+        //            }
+        //        }*/
+        //    }
+        //}
 
-        [Fact]
-        public void TestWithEdgeDriver()
-        {
-            using (var driver = new EdgeDriver
-                  (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
-            {
-                driver.Navigate().GoToUrl
-                 (@"https://localhost:44334/Applications/");
-                IWebElement tableElement = driver.FindElement(By.TagName("tbody"));
-                IList<IWebElement> tableRow = tableElement.FindElements(By.TagName("tr"));
-                IList<IWebElement> rowTD;
-                foreach (IWebElement row in tableRow)
-                {
-                    rowTD = row.FindElements(By.TagName("td"));
-                    if (rowTD.Last().FindElement(By.Id("Details")) != null)
-                    {
-                        rowTD.Last().FindElement(By.Id("Details")).Click();
-                        break;
-                    }
-                }
-            }
-        }
+        //[Fact]
+        //public void TestWithEdgeDriver()
+        //{
+        //    using (var driver = new EdgeDriver
+        //          (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+        //    {
+        //        driver.Navigate().GoToUrl
+        //         (@"https://localhost:44334/Applications/");
+        //        IWebElement tableElement = driver.FindElement(By.TagName("tbody"));
+        //        IList<IWebElement> tableRow = tableElement.FindElements(By.TagName("tr"));
+        //        IList<IWebElement> rowTD;
+        //        foreach (IWebElement row in tableRow)
+        //        {
+        //            rowTD = row.FindElements(By.TagName("td"));
+        //            if (rowTD.Last().FindElement(By.Id("Details")) != null)
+        //            {
+        //                rowTD.Last().FindElement(By.Id("Details")).Click();
+        //                break;
+        //            }
+        //        }
+        //    }
+        //}
 
         [Fact]
         public void TestAllChromeDriverTests()
