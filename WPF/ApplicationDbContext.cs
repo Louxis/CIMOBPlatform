5118c21bbaa7019451fb9fdbe136000820591783
Jorge Moreira <32908579+Vexxenn@users.noreply.github.com>
2018-01-15 14:56:19 +0000

    Starting WPF

---
+++ WPF/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using CIMOBProject.Controllers;
+using CIMOBProject.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF {
+    class ApplicationDbContext : DbContext {
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<College> Colleges { get; set; }
+        public DbSet<CollegeSubject> CollegeSubjects { get; set; }
+        public DbSet<Help> Helps { get; set; }
+        public DbSet<Error> Errors { get; set; }
+        public DbSet<ApplicationStat> ApplicationStats { get; set; }
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Edital> Editals { get; set; }
+        public DbSet<BilateralProtocol> BilateralProtocols { get; set; }
+        public DbSet<Quizz> Quizzs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        public DbSet<CIMOBProject.Models.ApplicationStatHistory> ApplicationStatHistory { get; set; }
+
+        public ApplicationDbContext() : base(@"Server=(localdb)\mssqllocaldb;Database=PVLab10;Trusted_Connection=True;")
+        { }
+    }
+}
