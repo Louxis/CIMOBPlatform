c66f3f1cd137ad6ca94d8110ceb5bca269c51692
Jorge Moreira <32908579+Vexxenn@users.noreply.github.com>
2018-01-18 15:13:16 +0000

    WPF application screen

---
+++ BackOfficeWPF/Screens/ApplicationScreen.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BackOfficeWPF.Screens {
+    /// <summary>
+    /// Interaction logic for ApplicationScreen.xaml
+    /// </summary>
+    public partial class ApplicationScreen : UserControl {
+        ApplicationDbContext _db = new ApplicationDbContext();
+        public ApplicationScreen()
+        {
+            InitializeComponent();
+            applicationGrd.ItemsSource = _db.Applications.Select(a => new { a.Student.UserFullname, a.ApplicationStat.Name, a.BilateralProtocol1.Destination, a.CreationDate, a.FinalGrade}).ToList();
+            applicationGrd.IsSynchronizedWithCurrentItem = true;
+        }
+    }
+}
