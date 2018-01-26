008cabde69d80ab9749cde1a193b4c2e0822ab72
Zelxy <jcpereira.dev@gmail.com>
2018-01-26 01:46:45 +0000

    Translations

--- CIMOBProject/Models/Interview.cs
+++ CIMOBProject/Models/Interview.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,6 +20,8 @@ namespace CIMOBProject.Models
         public virtual Application Application { get; set; }
 
         [ValidateDay]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data da entrevista")]
         public DateTime InterviewDate { get; set; }
     }
 }
